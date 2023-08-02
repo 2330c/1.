34af370d18c091ba07e02635902181216fee6205
Clara <2330chwang@gmail.com>
2023-08-02 14:38:15 -0400

    modified brown6-4.py

--- brown6-4.py
+++ brown6-4.py
@@ -1,3 +1,5 @@
+import numpy
+
 # y = (9-9*x**2/25)**.5
 # True for any quadrant 1 point on the ellipse
 # x**2/25 + y**2/9 = 1
@@ -8,6 +10,25 @@ numbers = [0, .5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]
 
 # You can either initialize an accumulator
 # or construct a list
+rect_areas = []
 for number in numbers:
     y = (9-9*number**2/25)**.5
     print(y)
+
+print("The areas for each rectangle: ")
+for number in numbers:
+    a = ((9-9*number**2/25)**.5)*.5
+    print(a)
+
+print("The area of the ellipse: ")
+
+def sum (x):
+    return x + a
+
+def sigma(f):
+    acc = 0
+    for w in numpy.arange(0,4.5,0.5):
+        acc = acc + w
+    return acc
+
+print(sigma(a))
--- sigma.py
+++ sigma.py
@@ -19,3 +19,27 @@ def sigma(f,lb,ub):
 print("sigma(square,-9,9):",sigma(square,-9,9))
 
 print("sigma(lambda x: x**2-2*x+2),2,9):",sigma((lambda x: x**2-2*x+2),2,9))
+
+planets_au = [0.4,0.7,1,1.5,5.2,9.6,19.2,30,39.5] #Including Pluto
+
+def get_stats(planets_au):
+    avg_dist = sum(planets_au)/len(planets_au)
+    print("avg_dist", avg_dist)
+
+    #Using list comprehensions
+    square_diffs = [(x-avg_dist)**2 for x in planets_au]
+    print("square_diffs", square_diffs)
+
+    variance = sum(square_diffs)/len(square_diffs)
+    print("variance",variance)
+
+    stdev = variance**.5
+    print("std. dev.",stdev)
+
+get_stats(planets_au)
+
+bpm_before = [56,64,68,68,68,68,68,72,72,72,72,72,72,72,72,76,76,76,76,76,80,88]
+bpm_after = [80,80,84,84,88,88,88,92,92,92,96,96,100,100,100,104,104,104,108,108,112,112]
+
+get_stats(bpm_before)
+get_stats(bpm_after)
