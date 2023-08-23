001babbb71e46b256e65767960922ee1289bc991
Clara <2330chwang@gmail.com>
2023-08-23 11:59:32 -0400

    added ch13-computer_exercise.py

--- ch13-computer_exercise.py
+++ ch13-computer_exercise.py
@@ -1,1 +1,10 @@
-ch13-computer_exercise
+def sigma(f,lb,ub):
+    """Returns the sum of f(k) as k goes from lb to ub, inclusive"""
+    acc = 0
+    for k in range(lb,ub+1):
+        acc = acc + f(k)
+    return acc
+
+def add(n):
+    return 1/n
+
